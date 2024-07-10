f4db11554a8bfbb4d69ae4ce6bdc950eabab7421
Vijaykarande <vijaykarande28@gmail.com>
2024-07-10 22:45:55 +0530

    non static initilizer

---
+++ nonsinitilizer.java
@@ -0,0 +1,22 @@
+class nonsinitilizer
+{
+	{
+		System.out.println("Vijay Karande");
+	}
+	
+	{
+		System.out.println("Sanjay Karande");
+	}
+	
+	{
+		System.out.println("Subhash Karande");
+	}
+	
+	public static void main(String [] args)
+	{
+		System.out.println("Vijay");
+		nonsinitilizer v1=new nonsinitilizer();
+		nonsinitilizer v2=new nonsinitilizer();
+		System.out.println("Karande");
+	}
+}
