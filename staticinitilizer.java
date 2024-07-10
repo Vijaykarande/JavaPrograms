b75ece38065793a575ecfacadaf6843eda6141b4
Vijaykarande <vijaykarande28@gmail.com>
2024-07-10 22:41:41 +0530

    static initilizer

---
+++ staticinitilizer.java
@@ -0,0 +1,23 @@
+class staticinitilizer
+{
+	static int a=10;
+	
+	static{
+		System.out.println("Vijay Karande");
+	}
+	
+	
+	static{
+		System.out.println("Sanjay Karande");
+	}
+	
+	
+	static {
+		System.out.println("Subhash Karande");
+	}
+	public static void main(String [] args)
+	{
+		System.out.println(a);
+	System.out.println("Hello");
+	}
+}
