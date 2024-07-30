c3499776d81deb3d365eabb522e66ecba6320d26
Vijaykarande <vijaykarande28@gmail.com>
2024-07-30 22:19:05 +0530

    length array

---
+++ array4.java
@@ -0,0 +1,12 @@
+class array4
+{
+	public static void main(String [] args)
+	{
+		int arr[]={20,30,40,50,60,70,80,90};
+		
+		for(int i=0;i<=arr.length-1;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
