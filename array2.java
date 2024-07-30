1d3073eddf5abb358e3ab0b714a37598655d8625
Vijaykarande <vijaykarande28@gmail.com>
2024-07-30 22:02:11 +0530

    array & string

---
+++ array2.java
@@ -0,0 +1,15 @@
+class array2
+{
+	public static void main(String [] args)
+	{
+		int arr[]={20,30,40,50,60,700,90};
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+	}
+}
