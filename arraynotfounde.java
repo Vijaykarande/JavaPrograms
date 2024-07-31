6c983f7cb919ae6948b1541ae216002cc203e284
Vijaykarande <vijaykarande28@gmail.com>
2024-07-31 23:09:53 +0530

    null pointer exception

---
+++ arraynotfounde.java
@@ -0,0 +1,12 @@
+class arraynotfounde
+{
+	public static void main(String [] args)
+	{
+	int arr[]={12,23,45,67,89,45,67,89,9};
+	
+	for(int i=0;i<=arr.length-1;i++)
+	{
+		System.out.println(arr[916]);
+	}
+	}
+}
---
+++ nullpointerexception.java
@@ -0,0 +1,10 @@
+class nullpointerexception
+{
+	public static void main(String [] args)
+	{
+		int arr[]={12,34,56,78,99,800};
+		
+		arr=null;
+		System.out.println(arr[2]);
+	}
+}
