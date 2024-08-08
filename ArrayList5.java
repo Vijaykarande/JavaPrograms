aeef9bd1de67487539436addeec2f14f2da8e758
Vijaykarande <vijaykarande28@gmail.com>
2024-08-08 22:48:14 +0530

    ArrayList Methods

---
+++ ArrayList5.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+class ArrayList5
+{
+	public static void main(String [] args)
+	{
+		
+		ArrayList a1=new ArrayList();
+		a1.add(10);
+		a1.add(20);
+		a1.add(30);
+		a1.add(40);
+		a1.add(50);
+		a1.add(60);
+		
+		System.out.println(a1);
+		
+		
+		ArrayList a2=new ArrayList();
+		a2.add(10);
+		a2.add(20);
+		a2.add(30);
+		a2.add(40);
+		a2.clear();
+		
+		System.out.println(a2);
+		
+		//a1.removeAll(a2);
+		
+		//System.out.println(a1);
+		
+		a1.retainAll(a2);
+		System.out.println(a1);
+	}
+}
+
+		
+		
