dca5e7968ab4ffa9ee241775aad1787b18dfde4e
Vijaykarande <vijaykarande28@gmail.com>
2024-08-09 21:18:07 +0530

    ArrayList Method

---
+++ ArrayListgetmethod.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+class ArrayListgetmethod
+{
+	public static void main(String [] args)
+	{
+		ArrayList a1=new ArrayList();
+		a1.add(10);
+		a1.add(20);
+		a1.add(30);
+		a1.add(40);
+		a1.add(50);
+		System.out.println(a1.get(1));
+	}
+}
+
+	
